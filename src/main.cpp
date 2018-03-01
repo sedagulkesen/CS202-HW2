2cceab6953894ad3a5584bc3c6dce6605661b3b1
Seda Gulkesen <sedagulkesen@Sedas-MacBook-Pro.local>
2018-03-01 22:28:00 +0300

    main updated

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,40 @@
 #include <iostream>
-#include "AbBST.h"
-#include "AbBinaryNode.h"
+#include "Analysis.h"
+
+using namespace std;
 
 int main() {
 
- AbBST* myTree= new AbBST();
 
+    PbBST* yourTree=new PbBST();
 
-    myTree->insert(2);
-    myTree->insert(3);
+    yourTree->insert(40);
+    yourTree->insert(50);
+    yourTree->insert(45);
+    yourTree->insert(30);
+    yourTree->insert(60);
+    yourTree->insert(55);
+    yourTree->insert(20);
+    yourTree->insert(35);
+    yourTree->insert(10);
+    yourTree->insert(25);
+
+    if(yourTree->isValidBST()==1)
+    {
+    cout<<"This is a valid BST"<<endl;
+    }
+    else
+        cout<<"This is not a valid BST"<<endl;
+    
+    cout<<"The median is: "<<yourTree->medianOfBST()<<endl;
+   
+    Analysis *analysis = new Analysis;
+    analysis->performanceAnalysis();
+    analysis->heightAnalysis();
+
 
     return 0;
+
+
 
 }
